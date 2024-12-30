925a459ae6b227b9ce00f2477aa900415b478506
masterjowo <daniajaOK@gmail.com>
2024-12-30 19:45:30 +0700

    Simpan perubahan lokal sebelum pull

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,9 @@ import io.cucumber.junit.CucumberOptions;
 
 // E:\QA\QA1\simple_web_automation_saucedemo_com\src\test\resources\feature\End2End_Test.feature
 @RunWith(Cucumber.class)
+
 @CucumberOptions(features = "src/test/resources/features", // Lokasi file .feature
+                strict = true, // Menjalankan strict mode
                 glue = { "stepdefenitions" }, // Lokasi step definitions. Cucumber memahami bahwa ini merujuk ke package
                                               // Java stepdefenitions, bukan direktori file di sistem file.
                 plugin = {
